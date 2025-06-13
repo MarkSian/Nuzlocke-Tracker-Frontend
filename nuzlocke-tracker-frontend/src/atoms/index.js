21d78eff9c3aa36abea06bbb9f2de8366f355afb
MarkSian <merksianipar@gmail.com>
2025-06-13 08:27:28 -0600

    Update index.js

--- nuzlocke-tracker-frontend/src/atoms/index.js
+++ nuzlocke-tracker-frontend/src/atoms/index.js
@@ -6,10 +6,8 @@ export const currentRunAtom = atom(null);
 export const pokemonStatsAtom = atom({}); // For storing Pokémon stats fetched from the API
 
 // Additional state for encounter modifications and inputs
-export const modifiedEncountersAtom = atom([]); // For storing modified encounters
 export const newEncounterAtom = atom({ routeName: '', pokemonName: '', nickname: '', nature: '', level: '' });
 export const newBossAtom = atom(''); // For adding new boss encounters or gym leaders
 export const newRivalAtom = atom(''); // For adding new rival encounters
 export const newRunGameAtom = atom(''); // For adding new game versions
-export const newRunStarterAtom = atom(''); // For adding new starter Pokémon
 export const newBadgeAtom = atom(''); // For adding new badges
